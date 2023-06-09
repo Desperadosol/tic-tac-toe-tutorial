5cc26dab5f9497dca837868a91b92a5fbbb3b11c
Desperadosol <igor.homichenko123@gmail.com>
2023-06-09 16:23:54 +0200

    3/5 in extra tasks

--- src/App.js
+++ src/App.js
@@ -30,24 +30,26 @@ function Board({xIsNext, squares, onPlay}) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  let board = [];
+  const sideSize = 3;
+  for (let i = 0; i < sideSize; i++) {
+    let entry = [];
+    for (let j = i*sideSize; j < (i+1)*sideSize; j++){
+      entry.push(
+      <Square value={squares[j]} onSquareClick={() => handleClick(j)} />
+      );
+    }
+    board.push(
+    <div key={i} className="board-row">
+      {entry}
+    </div>
+    );
+  }
+  
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {board}
     </>
   );
 }
@@ -57,6 +59,7 @@ export default function Game() {
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
+  const [isAscending, setIsAscending] = useState(true);
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0,currentMove + 1), nextSquares];
@@ -68,7 +71,12 @@ export default function Game() {
     setCurrentMove(nextMove);
   }
 
-  const moves = history.map((squares, move) => {
+  function handleSortToggle() {
+    setIsAscending(!isAscending);
+  }
+
+  const oldHistory = history.slice(0, history.length - 1);
+  const moves = oldHistory.map((squares, move) => {
     let description;
     if (move > 0) {
       description = 'Go to move #' + move;
@@ -83,13 +91,21 @@ export default function Game() {
     );
   });
 
+  const sortedMoves = isAscending ? moves : [...moves].reverse();
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{moves}</ol>
+        <ol>{sortedMoves}</ol>
+        <span>You are at move #{history.length - 1}</span>
+      </div>
+      <div className="game-info">
+        <button onClick={handleSortToggle}>
+          {isAscending ? 'Sort Descending' : 'Sort Ascending'}
+        </button>
       </div>
     </div>
   );
